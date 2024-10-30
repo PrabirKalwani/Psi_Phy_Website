a76b9ec7709a523e1d2df64d3b7e44d0e677db2c
PrabirKalwani <prabir.kalwani@gmail.com>
2024-10-30 15:56:13 +0530

    Fix : Removed .next folder

--- client/middleware.ts
+++ client/middleware.ts
@@ -4,15 +4,12 @@ import type { NextRequest } from 'next/server'
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl
 
-  // Protected routes
   const protectedRoutes = ['/dashboard']
 
-  // Check if the requested route is protected
   if (protectedRoutes.includes(pathname)) {
     const token = req.cookies.get('access_token')?.value
 
     if (!token) {
-      // If no token, redirect to the login page
       return NextResponse.redirect(new URL('/login', req.url))
     }
 
@@ -26,15 +23,12 @@ export function middleware(req: NextRequest) {
     })
       .then((response) => {
         if (response.ok) {
-          // If the token is valid, allow the request to continue
           return NextResponse.next()
         } else {
-          // If token verification fails, redirect to login
           return NextResponse.redirect(new URL('/login', req.url))
         }
       })
       .catch((error) => {
-        // In case of any error (e.g., network issue), redirect to login
         console.error('Error verifying token:', error)
         return NextResponse.redirect(new URL('/login', req.url))
       })
